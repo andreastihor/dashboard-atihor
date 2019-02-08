77fa1e6ca320a17e52cb233e0b29ac48d2cca31d
Taralite <taralite@Taralites-MacBook-Air.local>
2019-02-08 15:56:43 +0700

    validate cookie

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,15 +2,32 @@ import React , {Component} from 'react'
 import {NavLink} from 'react-router-dom'
 
 class NavBar extends Component {
+
+  onClick = event => {
+    document.cookie = "user =; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
+  }
+
   render () {
-    return (
-      <React.Fragment>
-      <NavLink to = "/"> <button>Home</button></NavLink>
-      <NavLink to = "/login"> <button>Login</button> </NavLink>
-      <NavLink to = "/createPost"> <button>CreatePost</button></NavLink>
-      <br/> <br/> <br/>
-      </React.Fragment>
-    )
+    if (document.cookie) {
+      return (
+          <React.Fragment>
+          <NavLink to = "/"> <button>Home</button></NavLink>
+          <NavLink to ="/"> <button onClick = {this.onClick}>Logout</button> </NavLink>
+          <NavLink to = "/createPost"> <button>CreatePost</button></NavLink>
+          <br/> <br/> <br/>
+          </React.Fragment>
+      )
+    }
+    else {
+      return (
+        <React.Fragment>
+        <NavLink to = "/"> <button>Home</button></NavLink>
+        <NavLink to = "/login"> <button>Login</button> </NavLink>
+        <br/> <br/> <br/>
+        </React.Fragment>
+      )
+    }
+
   }
 }
 
