a316178a9ebea9b988e62e88ab9525e8f71b4621
Taralite <taralite@Taralites-MacBook-Air.local>
2019-02-08 23:59:56 +0700

    added detail post

---
+++ src/components/detailPost.jsx
@@ -0,0 +1,68 @@
+import React , {Component} from 'react'
+import Header from './header'
+import axios from 'axios'
+import NotAuthorize from './Not Authorize'
+
+class detailPost extends Component {
+
+  state = {
+    id: null,
+    title : '',
+    content : '',
+    tags : []
+  };
+
+  componentDidMount () {
+    var id =this.props.match.params.id;
+    axios.get('http://localhost:3000/'+id)
+    .then(res => {
+      this.setState({
+        id : res.data.id,
+        title : res.data.title,
+        content : res.data.content,
+        tags : res.data.tags
+      })
+    });
+  }
+  routeChange = () => {
+      let path = `/${this.state.id}/update`;
+      this.props.history.push(path);
+  }
+  delete = () => {
+        console.log('delete here');
+  }
+
+  nameTag = () => {
+    const tags = this.state.tags.map(tag => {
+      return tag.name
+    })
+    return tags.join(',')
+  }
+render () {
+  if (document.cookie) {
+    return (
+      <div>
+      <Header/>
+
+      <label htmlFor="title">Id : </label>
+      <label htmlFor="title">{this.state.id}</label> <br/>
+      <label htmlFor="title">Title : </label>
+      <label htmlFor="title">{this.state.title}</label> <br/>
+      <label htmlFor="content">Content : </label>
+      <label htmlFor="title">{this.state.content}</label> <br/>
+      <label htmlFor="tags">Tags : </label>
+      <label htmlFor="title">{this.nameTag()}</label> <br/>
+      <button onClick = {this.routeChange} >Update</button>
+      <button onClick = {this.delete}>Delete</button>
+      </div>
+    )
+  }
+  return (
+    <div>
+    <NotAuthorize/>
+    </div>
+  )
+
+}
+}
+export default detailPost
